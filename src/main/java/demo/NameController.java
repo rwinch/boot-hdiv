a35c34ea7107044e1e9801c74c3ed5e99d116399
Rob Winch <rwinch@gopivotal.com>
2014-09-26 16:45:43 -0500

    initial

---
+++ src/main/java/demo/NameController.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2002-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class NameController {
+
+    @RequestMapping(value="/",method=RequestMethod.POST)
+    public String view(@ModelAttribute Name name) {
+        return "view";
+    }
+
+    @RequestMapping("/")
+    public String index(@ModelAttribute Name name) {
+        return "index";
+    }
+}
